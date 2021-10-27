b53b40a6f1ddd13aedfb3c1940995acd59304a72
ymei9 <ymei9@bu.edu>
2021-10-27 10:07:01 -0400

    02

--- sessions/02-distance-hclust/02-distance-hclust-setup.py
+++ sessions/02-distance-hclust/02-distance-hclust-setup.py
@@ -28,14 +28,13 @@ from sklearn.preprocessing import StandardScaler
 # YOUR_BILLING_PROJECT = ""
 # cars = pd.read_gbq(SQL, YOUR_BILLING_PROJECT)
 
-
+cars = pd.read_csv('/Users/yuxuanmei/Documents/GitHub/BA820-Fall-2021/datasets/cars.csv')
 
 # useful code snippets below ---------------------------------
 
 
 # filtered cars columns
 # ['mpg', 'cyl', 'disp', 'hp', 'drat', 'wt', 'qsec', 'vs', 'am', 'gear','carb']
-
 
 
 # a plot we will see later 
@@ -63,3 +62,52 @@ from sklearn.preprocessing import StandardScaler
 #              leaf_font_size=10)
   
 # plt.show()
+
+cars.set_index('model', inplace=True)
+cars.describe().T
+
+# keep only the columns of interest
+cars2  = cars[['mpg', 'cyl', 'disp', 'hp', 'drat', 'wt', 'qsec', 'vs', 'am', 'gear','carb']]
+
+# cars distance - passing in the numpy array from the pandas frame via values
+cdist = pdist(cars2.values)
+
+# squareform --- visualize
+sns.heatmap(squareform(cdist), cmap='Reds')
+plt.show()
+
+# our first cluster!
+hc1 = linkage(cdist)
+hc1
+
+# our first dendrogram!
+dendrogram(hc1, labels=cars.index)
+plt.show()
+
+# a second plot
+
+DIST = 80
+plt.figure(figsize=(5,6))
+dendrogram(hc1, labels = cars.index, orientation = "left", color_threshold = DIST)
+
+plt.axvline(x=DIST, c='grey', lw=1, linestyle='dashed')
+plt.show()
+
+# how to identify the clusters
+fcluster(hc1, 2, criterion = 'maxclust')
+cars2['cluster1']  = fcluster(hc1, 2, criterion = 'maxclust')
+cars2.head(3)
+
+# 'profiling'
+cars2.cluster1.value_counts()
+
+# distance assignment
+c2 = fcluster(hc1, 80, criterion = 'distance')
+cars2['cluster2'] = c2
+cars2.head()
+
+# different linkage methods
+linkage(cars2.values, method='')
+
+cars2.describe()
+ 
